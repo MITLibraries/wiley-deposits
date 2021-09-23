d45962e7a7422e74867c7e75819b23c747fa95cc
Eric Hanson <ehanson@mit.edu>
2021-09-23 15:07:46 -0400

    Add S3 class
    * Add S3 class with put_file method
    * Add correspondinig unit test and fixtures
    * Add moto dependency for testing
    
    * https://mitlibraries.atlassian.net/browse/DLSPP-110

---
+++ tests/test_s3.py
@@ -0,0 +1,13 @@
+from awd.s3 import S3
+
+
+def test_s3_put_file(mocked_s3):
+    s3 = S3()
+    assert "Contents" not in s3.client.list_objects(Bucket="awd")
+    s3.put_file(
+        str({"metadata": {"key": "dc.title", "value": "A Title"}}),
+        "awd",
+        "test.json",
+    )
+    assert len(s3.client.list_objects(Bucket="awd")["Contents"]) == 1
+    assert s3.client.list_objects(Bucket="awd")["Contents"][0]["Key"] == "test.json"
