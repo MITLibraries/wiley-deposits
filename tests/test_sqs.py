a9a1e0e3c645cc844a85566c2a8679215ab39f02
Eric Hanson <ehanson@mit.edu>
2021-10-20 16:12:34 -0400

    Update based on further discussion in #14

--- tests/test_sqs.py
+++ tests/test_sqs.py
@@ -1,17 +1,32 @@
 import boto3
+import pytest
+from botocore.exceptions import ClientError
 from moto import mock_sqs
 
 from awd import sqs
 
 
 @mock_sqs
-def test_sqs_send(sqs_client, dss_message_attributes_example, dss_message_body_example):
+def test_sqs_send_success(
+    sqs_client, dss_message_attributes_example, dss_message_body_example
+):
     sqs = boto3.resource("sqs", region_name="us-east-1")
     sqs_queue = sqs.create_queue(QueueName="mock-queue")
     response = sqs_client.send(
         sqs_queue.url, dss_message_attributes_example, dss_message_body_example
     )
     assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+
+@mock_sqs
+def test_sqs_send_failure(
+    sqs_client, dss_message_attributes_example, dss_message_body_example
+):
+    with pytest.raises(ClientError):
+        response = sqs_client.send(
+            "non-existent", dss_message_attributes_example, dss_message_body_example
+        )
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
 
 
 def test_create_dss_message_attributes(dss_message_attributes_example):
