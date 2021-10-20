126d9cbb39b88dcdd1b3516a360cfc4b21e48b1f
Eric Hanson <ehanson@mit.edu>
2021-10-20 10:29:54 -0400

    Update based on discussion in #11

--- awd/wiley.py
+++ awd/wiley.py
@@ -1,4 +1,8 @@
+import logging
+
 import requests
+
+logger = logging.getLogger(__name__)
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
@@ -6,6 +10,8 @@ headers = {
 }
 
 
-def get_wiley_pdf(url, doi):
-    """Get PDF from Wiley server based on a DOI."""
-    return requests.get(f"{url}{doi}", headers=headers)
+def get_wiley_response(url, doi):
+    """Get response from Wiley server based on a DOI."""
+    response = requests.get(f"{url}{doi}", headers=headers)
+    logger.info(f"Response retrieved from Wiley server for {doi}")
+    return response
--- tests/test_wiley.py
+++ tests/test_wiley.py
@@ -3,5 +3,5 @@ from awd import wiley
 
 def test_get_wiley_pdf(web_mock, wiley_pdf):
     doi = "10.1002/term.3131"
-    response = wiley.get_wiley_pdf("http://example.com/doi/", doi)
+    response = wiley.get_wiley_response("http://example.com/doi/", doi)
     assert response.content == wiley_pdf
