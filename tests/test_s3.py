c2c3e67a0777905f026b22487fcbba17a376292b
Eric Hanson <ehanson@mit.edu>
2021-10-25 16:30:38 -0400

    Refactor S3 code per discussion on #13

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,3 +1,4 @@
+from awd import s3
 from awd.s3 import S3
 
 
@@ -11,3 +12,11 @@ def test_s3_put_file(s3_mock):
     )
     assert len(s3.client.list_objects(Bucket="awd")["Contents"]) == 1
     assert s3.client.list_objects(Bucket="awd")["Contents"][0]["Key"] == "test.json"
+
+
+def test_create_package_files_dict():
+    package_files = s3.create_package_files_dict("111.1-111", {"key": "value"}, b"")
+    assert package_files == [
+        {"file_content": {"key": "value"}, "file_name": "111.1-111.json"},
+        {"file_content": b"", "file_name": "111.1-111.pdf"},
+    ]
